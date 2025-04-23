806f891bf5f24035fc7caddbf9fcccdf7649d82a
Mykola Stesiuk <mikalaj.krutak@gmail.com>
2025-04-23 16:09:23 +0200

    make build

---
+++ index.72974e9a.js
@@ -0,0 +1,2 @@
+var t=document.getElementById("form"),e=document.getElementById("form_input");t.addEventListener("submit",function(t){t.preventDefault(),e.value="",e.classList.add("subscribe__input--submitted"),window.scrollTo({top:0,behavior:"smooth"})});
+//# sourceMappingURL=index.72974e9a.js.map
